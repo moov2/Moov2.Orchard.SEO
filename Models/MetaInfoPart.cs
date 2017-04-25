2d92612614c1d509a836cbd96344e04699e4bdbb
Peter Keating <peter@moov2.com>
2017-04-25 09:53:57 +0100

    Fix issue with building module with older version of MSBuild

--- Models/MetaInfoPart.cs
+++ Models/MetaInfoPart.cs
@@ -5,11 +5,13 @@ namespace Moov2.Orchard.SEO.Models
     public class MetaInfoPart : ContentPart<MetaInfoPartRecord>
     {
         public string Description {
-            get => Retrieve(x => x.Description); set => Store(x => x.Description, value);
+            get { return Retrieve(x => x.Description); }
+            set { Store(x => x.Description, value); }
         }
 
         public string Keywords {
-            get => Retrieve(x => x.Keywords); set => Store(x => x.Keywords, value);
+            get { return Retrieve(x => x.Keywords); }
+            set { Store(x => x.Keywords, value); }
         }
     }
 }
