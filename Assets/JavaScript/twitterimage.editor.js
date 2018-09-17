f049702f8e1c2bdab2335a000fb184b646ba1bf3
Joe B <Joe@Moov2.com>
2018-09-17 10:48:09 +0100

    Update OG and twitter image fields to use media picker

---
+++ Assets/JavaScript/twitterimage.editor.js
@@ -0,0 +1,60 @@
+﻿(function () {
+    /**
+     * Opens media library picker.
+     */
+    var openMediaPicker = function (e) {
+        e.preventDefault();
+
+        var adminIndex = location.href.toLowerCase().indexOf("/admin/"),
+            url;
+
+        if (adminIndex === -1) {
+            return;
+        }
+
+        url = location.href.substr(0, adminIndex) + "/Admin/Orchard.MediaLibrary?dialog=true";
+
+        $.colorbox({
+            href: url,
+            iframe: true,
+            reposition: true,
+            width: '90%',
+            height: '90%',
+            onLoad: function () {
+                // hide the scrollbars from the main window
+                $('html, body').css('overflow', 'hidden');
+            },
+            onClosed: function () {
+                var adminIndex = location.href.toLowerCase().indexOf("/admin/")
+                var selectedData = $.colorbox.selectedData;
+
+                $('html, body').css('overflow', '');
+
+                if (selectedData.length === 0) {
+                    return;
+                }
+
+                // The media doesn't seem to be a local resource, 
+                // pass its current resource path to setMedia
+                if (selectedData[0].resource[0] !== "/")
+                    setMedia(selectedData[0].resource);
+                else
+                    setMedia(location.href.substr(0, adminIndex) + selectedData[0].resource);
+            }
+        });
+    };
+
+    var setMedia = function (url) {
+        document.querySelector('.js-twitter-image .js-favicon-url-input').value = url;
+    };
+
+    /**
+     * Initialises favicon editor
+     */
+    var init = function () {
+        var $mediaLibrary = document.querySelector('.js-twitter-image .js-open-media-library');
+        $mediaLibrary.addEventListener('click', openMediaPicker);
+    };
+
+    init();
+})();
--- ResourceManifest.cs
+++ ResourceManifest.cs
@@ -5,6 +5,8 @@ namespace Moov2.Orchard.Featured {
         public void BuildManifests(ResourceManifestBuilder builder) {
             var manifest = builder.Add();
             manifest.DefineScript("FaviconEditor").SetUrl("favicon.editor.min.js", "favicon.editor.js").SetDependencies(new string [] { "JQuery", "jQueryColorBox" });
+            manifest.DefineScript("OGImageEditor").SetUrl("ogimage.editor.min.js", "ogimage.editor.js").SetDependencies(new string[] { "JQuery", "jQueryColorBox" });
+            manifest.DefineScript("TwitterImageEditor").SetUrl("twitterimage.editor.min.js", "twitterimage.editor.js").SetDependencies(new string[] { "JQuery", "jQueryColorBox" });
         }
     }
 }
